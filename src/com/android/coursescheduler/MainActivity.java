7678af2b6c18e0da42796eb37673ecb7c4c16a0c
Andreas Adolfsson <wazzur@users.noreply.github.com>
2015-03-25 06:07:30 -0400

    refactoring

--- src/com/android/coursescheduler/MainActivity.java
+++ src/com/android/coursescheduler/MainActivity.java
@@ -30,16 +30,16 @@ import android.widget.TextView;
  public class MainActivity extends Activity {
 
 	Schedule s;	// user schedule variable
-	TextView view;		
+	TextView gpaText;		
 	final Context context = this;
-	int credits, sem;	
-	Class nextSem[];	// next semester of classes
-	LinearLayout L, LL;	// these variables are our layouts
-	LinearLayout.LayoutParams LP;	// LP = Layout Parameters
-	Button cb, b, makeSched;	// credits button, general class button, and make schedule buttons
-	GradientDrawable d;
-    Database DB;
-	boolean sExist;		// to check if schedule exists to remake.
+	int credits, semester;	
+	Class nextSemester[];	// next semester of classes
+	LinearLayout tempLayout, layout;	// these variables are our layouts
+	LinearLayout.LayoutParams layoutParams;	
+	Button creditsButton, courseButton, makeSchedule;	// credits button, general class button, and make schedule buttons
+	GradientDrawable gDraw;
+    Database database;
+	boolean clearSchedule;		// to check if schedule exists to remake.
 				
     protected void onCreate(Bundle savedInstanceState) {
 
@@ -51,12 +51,12 @@ import android.widget.TextView;
 
         
         //credits button
-        cb.setOnClickListener(new View.OnClickListener() {
+        creditsButton.setOnClickListener(new View.OnClickListener() {
 			@SuppressLint("InflateParams") @Override
 			public void onClick(View v) {
 				// sets up popup variables
-				LayoutInflater li = LayoutInflater.from(context);
-				View promptsView = li.inflate(R.layout.prompts, null);
+				LayoutInflater layoutInflater = LayoutInflater.from(context);
+				View promptsView = layoutInflater.inflate(R.layout.prompts, null);
 				AlertDialog.Builder alert = new AlertDialog.Builder(context);
 				alert.setView(promptsView);
 				final EditText userInput;
@@ -70,7 +70,7 @@ import android.widget.TextView;
 					    		// TODO: error checking.
 					    	Editable inpt = userInput.getText();
 					    	credits = Integer.parseInt(inpt.toString());
-					    	cb.setText("MinCredits: " + credits);
+					    	creditsButton.setText("MinCredits: " + credits);
 					    }
 					  })
 					.setNegativeButton("Cancel",
@@ -85,17 +85,17 @@ import android.widget.TextView;
 		});
         
         //make schedule button
-        makeSched.setOnClickListener(new View.OnClickListener() {
+        makeSchedule.setOnClickListener(new View.OnClickListener() {
 			@Override
 			public void onClick(View v) {
 				// checks if shedule exists and needs to be removed
-				if(sExist){
-					LL.removeView(L);
+				if(clearSchedule){
+					layout.removeView(tempLayout);
 					s.reset();
 				}
 				// makes schedule based on credits variable
 				makeButtons(s.makeSchedule(credits));
-				sExist = true;	// sets schedule to exist for future presses
+				clearSchedule = true;	// ensures schedule is cleared next time user clicks schedule
 			}
 		});
     }
@@ -114,22 +114,21 @@ import android.widget.TextView;
                         + " (c_major_name)"
                         + " VALUES ('Computer Science');");
             }
-
         }
         */
 
-        makeSched = (Button) findViewById(R.id.makeSchedule);
-        LP = new LinearLayout.LayoutParams(
+        makeSchedule = (Button) findViewById(R.id.makeSchedule);
+        layoutParams = new LinearLayout.LayoutParams(
 	            LinearLayout.LayoutParams.WRAP_CONTENT,
 	            LinearLayout.LayoutParams.WRAP_CONTENT);
-        LL = (LinearLayout) findViewById(R.id.LL);
-        view  = (TextView) findViewById(R.id.gpaView);
-        cb = (Button) findViewById(R.id.credBtn);
-        d = new GradientDrawable();
-		d.setShape(GradientDrawable.RECTANGLE);
-		d.setStroke(5, Color.parseColor("#540115"));
-		d.setColor(Color.parseColor("#CDC092"));
-		sExist = false;	// first time making shedule.
+        layout = (LinearLayout) findViewById(R.id.LL);
+        gpaText  = (TextView) findViewById(R.id.gpaView);
+        creditsButton = (Button) findViewById(R.id.credBtn);
+        gDraw = new GradientDrawable();
+		gDraw.setShape(GradientDrawable.RECTANGLE);
+		gDraw.setStroke(5, Color.parseColor("#540115"));
+		gDraw.setColor(Color.parseColor("#CDC092"));
+		clearSchedule = false;	// nothing to clear first time making schedule.
 		credits = 12;	// standard schedule floor
         String contents ="";   	// empty file contents
 	
@@ -147,68 +146,68 @@ import android.widget.TextView;
 		// this function/method creates the buttons for each class in our schedule
 		
 		//initializes variables
-    	sem = 0;
-    	LP.setMargins(20, 20, 20, 20);
-    	L = new LinearLayout(this);		// L = layout
-		L.setOrientation(LinearLayout.VERTICAL);
-    	TableLayout T = new TableLayout(this);	// table of buttons
-    	T.setLayoutParams(new TableLayout.LayoutParams(10,2));	// 2 per row
-    	TableRow tr = null;		// maximum of 10 rows (should be excessive)
-    	int bCount = 0;
+    	semester = 0;
+    	layoutParams.setMargins(20, 20, 20, 20);
+    	tempLayout = new LinearLayout(this);		// L = layout
+		tempLayout.setOrientation(LinearLayout.VERTICAL);
+    	TableLayout table = new TableLayout(this);	// table of buttons
+    	table.setLayoutParams(new TableLayout.LayoutParams(10,2));	// 2 per row
+    	TableRow row = null;		// maximum of 10 rows (should be excessive)
+    	int buttonCounter = 0;
     	
     	// iterates through the schedule to print the buttons to layout
     	// exit if schedule nulls for any reason, this should never happen
-    	for(int s=0; s<schedule.length; s++){
-    		if(schedule[s] == null){	break;	}	
+    	for(int sem=0; sem<schedule.length; sem++){
+    		if(schedule[sem] == null){	break;	}	
     		semBreak();								// creates a visual break between semesters
-	    	for(int i=0; i<schedule[s].length; i++){	// iterates through each semester and prints
-	    		if(bCount%2==0){	// corrects UI alignment if button ended in an odd number
-	    			tr = new TableRow(this);
-	    			tr.setPadding(5, 5, 5, 5);
-	    			tr.setDividerDrawable(getWallpaper());
+	    	for(int course=0; course<schedule[sem].length; course++){	// iterates through each semester and prints
+	    		if(buttonCounter%2==0){	// corrects UI alignment if button ended in an odd number
+	    			row = new TableRow(this);
+	    			row.setPadding(5, 5, 5, 5);
+	    			row.setDividerDrawable(getWallpaper());
 	    		}
-	    		if(schedule[s][i] != null){    			// verifies class existance
-		    		 b = new Button(this);				// creates new button
-		             b.setText(schedule[s][i].getCode());    	// sets button name to class code
-		             b.setId(i);						// sets button reference id
-		             b.setGravity(Gravity.CENTER);		// centralizes button gravity
-		             b.setBackground(d);				// sets background
-		             tr.addView(b, 185, 115);			// adds button to our table row
-		             bCount++;							// incremenets button counter
+	    		if(schedule[sem][course] != null){    			// verifies class existance
+		    		 courseButton = new Button(this);				// creates new button
+		             courseButton.setText(schedule[sem][course].getCode());    	// sets button name to class code
+		             courseButton.setId(course);						// sets button reference id
+		             courseButton.setGravity(Gravity.CENTER);		// centralizes button gravity
+		             courseButton.setBackground(gDraw);				// sets background
+		             row.addView(courseButton, 185, 115);			// adds button to our table row
+		             buttonCounter++;							// incremenets button counter
 		             
 		             // adds the row to the table
-		             if(bCount%2==0 && bCount != 0){	T.addView(tr, LP);	} 
+		             if(buttonCounter%2==0 && buttonCounter != 0){	table.addView(row, layoutParams);	} 
 		             
 		             //allows click-ability of dynamically creates buttons
-		             final int ss=s;
-		             final int ii=i;
-		             b.setOnClickListener(new View.OnClickListener() {
+		             final int s=sem;
+		             final int c=course;
+		             courseButton.setOnClickListener(new View.OnClickListener() {
 						@Override
 						public void onClick(View v) {
-							classBtn(schedule[ss][ii]);
+							classBtn(schedule[s][c]);
 						}
 					});
 	    		}
 	    	}	// corrects UI if necessary
-	    	if(bCount%2==1){	
-				b = new Button(this);
-				b.setVisibility(View.GONE);
-				tr.addView(b);
-				T.addView(tr, LP);	
-				bCount++;
+	    	if(buttonCounter%2==1){	
+				courseButton = new Button(this);
+				courseButton.setVisibility(View.GONE);
+				row.addView(courseButton);
+				table.addView(row, layoutParams);	
+				buttonCounter++;
     		}
-	    	L.addView(T, LP);
-	    	T = new TableLayout(this);
-	    	T.setLayoutParams(new TableLayout.LayoutParams(10,2));
+	    	tempLayout.addView(table, layoutParams);
+	    	table = new TableLayout(this);
+	    	table.setLayoutParams(new TableLayout.LayoutParams(10,2));
     	}	// corrects UI if necessary
-    	if(bCount%2==1){	
-			b = new Button(this);
-			b.setVisibility(View.GONE);
-			tr.addView(b);
-			T.addView(tr, LP);	
-			L.addView(T, LP);
+    	if(buttonCounter%2==1){	
+			courseButton = new Button(this);
+			courseButton.setVisibility(View.GONE);
+			row.addView(courseButton);
+			table.addView(row, layoutParams);	
+			tempLayout.addView(table, layoutParams);
 		}
-    	LL.addView(L, LP);
+    	layout.addView(tempLayout, layoutParams);
     	addText("Graduation!");	// prints graduation message
     }
     
@@ -234,7 +233,7 @@ import android.widget.TextView;
 			    	Editable inpt = userInput.getText();
 			    	c.setGrade(inpt.toString());
 			    	if(c.getGrade() != null){
-			    		view.setText("GPA: "+s.calcGPA());
+			    		gpaText.setText("GPA: "+s.calcGPA());
 			    	}
 			    }
 			  })
@@ -253,27 +252,28 @@ import android.widget.TextView;
     	t.setGravity(Gravity.CENTER);
     	t.setTextColor(Color.parseColor("#CDC092"));
 		t.setText(txt);
-		L.addView(t, LP);
+		tempLayout.addView(t, layoutParams);
     }
     
     void semBreak(){
     	String txt;
-    	int c = (1+(sem/2));
-    	if(sem%2==0){	txt = "Fall Semester " + c;
+    	int c = (1+(semester/2));
+    	if(semester%2==0){	txt = "Fall Semester " + c;
     	}else{	txt = "Spring Semester "+c;	}
     	TextView t = new TextView(this);
     	t.setPadding(75, 0, 0, 0);
     	t.setGravity(Gravity.CENTER);
     	t.setTextColor(Color.parseColor("#CDC092"));
 		t.setText(txt);
-		L.addView(t, LP);
-		sem++;
+		tempLayout.addView(t, layoutParams);
+		semester++;
     }
     
     public String readData() throws IOException {      
     	// reads datafile and returns it as a string variable
+    	// TODO: CHANGE THIS TO READ FROM DATABASE
     	
-
+    	
     	String str="";
     	StringBuffer buf = new StringBuffer();
     	// finds file in raw folder under the name of "data"
