6a08855a6a2013e526a249c059d63e0f28ea52c1
Michael Barrera <thedefectiverobot@gmail.com>
2015-04-20 15:34:19 -0400

    Renamed nav drawer

--- src/com/android/coursescheduler/NavigationDrawerFragment.java
+++ src/com/android/coursescheduler/NavigationDrawerFragment.java
@@ -103,7 +103,7 @@ public class NavigationDrawerFragment extends Fragment {
                 android.R.layout.simple_list_item_activated_1,
                 android.R.id.text1,
                 new String[]{
-                        "test1",
+                        "Choose Major",
                         "test drawer 2",
 
                 }));
--- src/com/android/coursescheduler/Schedule.java
+++ src/com/android/coursescheduler/Schedule.java
@@ -1,5 +1,6 @@
 package com.android.coursescheduler;
 
+import java.lang.reflect.Array;
 import java.util.ArrayList;
 
 public class Schedule {
